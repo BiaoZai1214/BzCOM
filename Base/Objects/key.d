481ce7005a4f5b389182e6db052c0c011b8cd458
BiaoZai <127381332+BiaoZai1214@users.noreply.github.com>
2026-04-01 19:05:54 +0800

    Delete Base directory

--- Base/Objects/key.d
+++
@@ -1,6 +0,0 @@
-.\objects\key.o: Hardware\KEY\key.c
-.\objects\key.o: Hardware\KEY\KEY.h
-.\objects\key.o: .\Start\stm32f10x.h
-.\objects\key.o: .\Start\core_cm3.h
-.\objects\key.o: E:\Software\Keil MDK\ARM\arm5_compiler\Bin\..\include\stdint.h
-.\objects\key.o: .\Start\system_stm32f10x.h
--- Base/Objects/tim1.d
+++
@@ -1,9 +0,0 @@
-.\objects\tim1.o: Hardware\TIM\tim1.c
-.\objects\tim1.o: Hardware\TIM\tim1.h
-.\objects\tim1.o: .\Start\stm32f10x.h
-.\objects\tim1.o: .\Start\core_cm3.h
-.\objects\tim1.o: E:\Software\Keil MDK\ARM\arm5_compiler\Bin\..\include\stdint.h
-.\objects\tim1.o: .\Start\system_stm32f10x.h
-.\objects\tim1.o: .\Hardware\USART\usart.h
-.\objects\tim1.o: E:\Software\Keil MDK\ARM\arm5_compiler\Bin\..\include\stdio.h
-.\objects\tim1.o: .\Hardware\LED\led.h
