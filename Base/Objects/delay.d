59d4773ece19ec6de27e7336698944e742b9b6cd
BiaoZai <127381332+BiaoZai1214@users.noreply.github.com>
2026-04-01 19:05:54 +0800

    Delete Base directory

--- Base/Objects/delay.d
+++
@@ -1,6 +0,0 @@
-.\objects\delay.o: User\Delay.c
-.\objects\delay.o: User\Delay.h
-.\objects\delay.o: .\Start\stm32f10x.h
-.\objects\delay.o: .\Start\core_cm3.h
-.\objects\delay.o: E:\Software\Keil MDK\ARM\arm5_compiler\Bin\..\include\stdint.h
-.\objects\delay.o: .\Start\system_stm32f10x.h
--- Base/Objects/timer.d
+++
@@ -1,7 +0,0 @@
-.\objects\timer.o: User\Timer.c
-.\objects\timer.o: .\Start\stm32f10x.h
-.\objects\timer.o: .\Start\core_cm3.h
-.\objects\timer.o: E:\Software\Keil MDK\ARM\arm5_compiler\Bin\..\include\stdint.h
-.\objects\timer.o: .\Start\system_stm32f10x.h
-.\objects\timer.o: User\Timer.h
-.\objects\timer.o: .\Hardware\LED\led.h
